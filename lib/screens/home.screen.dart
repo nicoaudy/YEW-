4c50056e5af776c8f9c630828979bd8c42f95dc2
Nico Audy <nicoaudy1994@gmail.com>
2020-07-28 10:32:03 +0700

    scaffold for home design

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -6,9 +6,8 @@ import 'package:yew/components/category_card.dart';
 class HomeScreen extends StatelessWidget {
   final Map<String, int> categories = {
     'Notes': 112,
-    'Work': 58,
-    'Home': 23,
-    'Complete': 31,
+    'Reminder': 3,
+    'Expenses': 0,
   };
 
   @override
@@ -58,7 +57,78 @@ class HomeScreen extends StatelessWidget {
                 );
               },
             ),
-          )
+          ),
+
+
+          SizedBox(height: 20.0),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30.0),
+            padding: EdgeInsets.all(30.0),
+            decoration: BoxDecoration(
+              color: Color(0xFFF5F7FB),
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Walk with dog',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '20-08-2020',
+                      style: TextStyle(
+                        color: Color(0xFFAFB4C6),
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  'Walk on the street with my favorite dog.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      '20-08-2020',
+                      style: TextStyle(
+                        color: Color(0xFFAFB4C6),
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Container(
+                      height: 50.0,
+                      width: 50.0,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF417BFB),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Icon(
+                        Icons.location_on,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
      ),
     );
