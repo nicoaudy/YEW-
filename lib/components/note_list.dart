79ab840893eeb4036f309a6518336230db32cba0
Nico Audy <nicoaudy1994@gmail.com>
2020-07-29 15:03:42 +0700

    bug WIP

--- lib/components/note_list.dart
+++ lib/components/note_list.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:yew/components/note_card.dart';
 import 'package:yew/providers/note.provider.dart';
 
 class NoteList extends StatelessWidget {
-  var notes = NoteProvider();
-
   @override
   Widget build(BuildContext context) {
+    final notes = Provider.of<NoteProvider>(context);
+
     return Container(
       height: MediaQuery.of(context).size.height,
       child: ListView.builder(
